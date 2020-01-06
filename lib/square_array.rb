5c71a549fd2f9e8fd4a9f13e7c86f8e75c9e5491
Steven Frasica <sfrasica1@gmail.com>
2020-01-06 02:22:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,1 @@
-def square_array(array)
-  square_numbers = []
-  counter = 0
 
-  while counter < array.length do
-    square_numbers << (array[counter]**2)
-    counter += 1
-  end
-  square_numbers
-end
